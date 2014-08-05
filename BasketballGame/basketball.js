6c394774ac75e3de604b9c1e3beaac2994d13fad
Lookuptothemoon <lruiz@afsenyc.org>
2014-08-05 17:00:22 -0400

    Worked on the Basketball project by adding background, hoop and basketball

--- BasketballGame/basketball.js
+++ BasketballGame/basketball.js
@@ -1,5 +1,4 @@
 var stage;
-//var tween;
 var text;
 
 function init() {
@@ -8,29 +7,39 @@ function init() {
     stage.enableMouseOver();
 
     var basketball = new createjs.Bitmap("images/basketball.png");
-    var hoop = new createjs.Bitmap("images/hoop.png");
-    hoop.x = 180;   //-65
-    hoop.y = -10;
-    hoop.alpha = 0.5;
+    basketball.scaleX = 0.3;
+    basketball.scaleY = 0.3;
+    basketball.x = -122;        // -122 & 833
+    basketball.y = -87;         //  -87 & 570
+    basketball.alpha = 0.5;
 
-    ballCanvas.addEventListener("click", function handleClick(){
-        createjs.Tween.get(basketball).to({x:884}, 2500, createjs.Ease.quadOut);
-        createjs.Tween.get(basketball).to({y:397}, 2000, createjs.Ease.bounceOut);
+    var hoop = new createjs.Bitmap("images/Hoop.png");
+    hoop.x = 322;
+    hoop.y = -50;
+    hoop.scaleY = 1.30;
+    hoop.scaleX = 1.05;
+
+    basketball.addEventListener("click", function handleClick(){
+        createjs.Tween.get(basketball).to({x:833}, 2500, createjs.Ease.quadOut);
+        createjs.Tween.get(basketball).to({y:570}, 2000, createjs.Ease.bounceOut);
     });
 
 
-    hoop.addEventListener("mouseover", function handleInteraction(event){
-        event.currentTarget.alpha = 1;
+    basketball.addEventListener("mouseover", function handleInteraction(event){
+        basketball.alpha = 1;
     });
-    hoop.addEventListener("mouseout", function other(event){
-        event.currentTarget.alpha = 0.5;
+    /*
+    basketball.addEventListener("mouseout", function other(event){
+        basketball.alpha = 0.5;
     });
+    */
+
 
     basketball.addEventListener("tick", function hit(event){
         var pt = basketball.localToLocal(0,0,hoop);
         if(hoop.hitTest(pt.x,pt.y)){
-            hoop.x = Math.floor((Math.random() * 723) - 82);
-            hoop.y = Math.floor((Math.random() * 256 - 38));
+            basketball.x = Math.floor((Math.random() * 833) - 122);
+            basketball.y = Math.floor((Math.random() * 570 - 87));
             text = new createjs.Text("YOU WIN!", "50px Arial", "orange");
             text.x = 430;
             text.y = 250;
@@ -38,8 +47,7 @@ function init() {
         }
     });
 
-    stage.addChild(basketball);
-    stage.addChild(hoop);
+    stage.addChild(basketball, hoop);
 
     createjs.Ticker.addEventListener("tick", tick);
 }
@@ -48,4 +56,6 @@ function tick(event) {
     stage.update();
 }
 
+init();
+init();
 init();
